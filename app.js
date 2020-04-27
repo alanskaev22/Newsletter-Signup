e8c69a238f9e25d4cf8ac10e5b0b53773287ae9c
Alan Skaev <askaev@rccl.com>
2020-04-27 19:31:19 -0400

    Added success.html page

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/', (req, res) => {
+    res.header("Content-Type", "text/html");
+    res.sendFile(__dirname + '/signup.html');
+});
 
+app.post("/", (req, res) => {
+    let email, firstName, lastName;
+    email = req.body.inputEmail;
+    firstName = req.body.firstName;
+    lastName = req.body.lastName;
+});
 
-app.listen(3000, (args) => console.log("Application in running on port 3000. Args: " + args));
+app.listen(3000, () => console.log("Application in running on port 3000."));
